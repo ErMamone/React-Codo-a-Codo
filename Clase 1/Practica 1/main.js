2ccaa1a613a0f21b4d96e9242f635ac2fb3f7646
meroz <ermamone@gmail.com>
2022-03-08 12:32:28 -0300

    Se agrega practica 1

---
+++ Clase 1/Practica 1/main.js
@@ -0,0 +1,9 @@
+/**
+ *   Imágenes
+ *   Enlaces internos y externos
+ *   formatos de textos
+ *   estructuras comentadas (que hacen una buena práctica)
+ *   uno o varios componentes de bootstrap
+ *   animaciones que pueden ser con css o js
+ *   incorporación de clases y id para darle formato
+ */
