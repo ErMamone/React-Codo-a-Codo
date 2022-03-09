fd633107a9d14e1f8a4e5c58cc18419cf9dba385
meroz <ermamone@gmail.com>
2022-03-09 19:13:47 -0300

    Practica con objetos js

--- Clase 1/Practica 1/main.js
+++ Clase 1/Practica 1/main.js
@@ -1,3 +1,22 @@
+class animal {
+    constructor(especie, edad, color){
+        this.especie=especie;
+        this.edad=edad;
+        this.color=color;
+    }
+}
+
+let perro = new animal("perro", 5, "marron");
+
+console.log(perro)
+
+
+
+
+
+
+
+
 /**
  *   Imágenes
  *   Enlaces internos y externos
